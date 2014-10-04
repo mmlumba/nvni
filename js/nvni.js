4fe7805e5831225f4e03f5bae8e07c2192991c1b
Tyrone Brim <tyrone.brim@gmail.com>
2014-10-04 03:12:31 -0400

    added carousel to page

--- js/nvni.js
+++ js/nvni.js
@@ -1,3 +1,27 @@
 $(function(){
   $('#mochiPicker').datepicker();
-})
+  $('#news-carousel').carousel({
+    interval: 4000
+  })
+    .on('slid.bs.carousel', function(){
+
+      var index = +$('.list-group li.active').attr('data-slide-to');
+      var listItems = $('.list-group li');
+      listItems.removeClass('active');
+
+      if(index >= listItems.size()-1){
+        index = 0;
+      }
+      else{
+        index++;
+      }
+
+      $('.list-group li[data-slide-to="' + index + '"]').addClass('active');
+    });
+
+  $('.list-group li').click(function(){
+    $('.list-group li').removeClass('active');
+    $(this).addClass('active');
+
+  });
+});
